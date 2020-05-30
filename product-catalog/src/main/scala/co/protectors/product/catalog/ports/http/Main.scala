16580ca1507653222a8d135cdf7fa09d537363c8
Giovanny Sissa <guiovannys@hotmail.es>
2020-05-30 15:46:58 -0500

    include CORS to allow any origin

--- product-catalog/src/main/scala/co/protectors/product/catalog/ports/http/Main.scala
+++ product-catalog/src/main/scala/co/protectors/product/catalog/ports/http/Main.scala
@@ -9,9 +9,11 @@ import co.protectors.product.catalog.ports.http.routes.Routes
 import io.circe.config.parser
 import org.http4s.server.{ Router, Server }
 import org.http4s.server.blaze.BlazeServerBuilder
-import org.http4s.server.middleware.{ AutoSlash, GZip, Timeout }
+import org.http4s.server.middleware.{ AutoSlash, CORS, CORSConfig, GZip, Timeout }
 import org.http4s.implicits._
 import cats.syntax.functor._
+
+import scala.concurrent.duration._
 
 object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] =
@@ -24,8 +26,16 @@ object AppResources {
       config <- Resource.liftF(parser.decodePathF[F, DefaultConfig]("app"))
       xa <- Transactor.acquire(config.database)
       implicit0(repo: ItemAlg[F]) = LiveItemRepository[F](xa).itemRepository
-      routes                      = Routes[F].endPoints
-      httpApp                     = Timeout(config.http.timeout)(GZip(AutoSlash(Router("/products" -> routes))))
+      // todo: we should provide a web proxy to avoid enable CORS
+      methodConfig = CORSConfig(
+        anyOrigin = true,
+        anyMethod = false,
+        allowedMethods = Some(Set("GET", "POST")),
+        allowCredentials = true,
+        maxAge = 1.day.toSeconds
+      )
+      routes  = CORS(Routes[F].endPoints, methodConfig)
+      httpApp = Timeout(config.http.timeout)(GZip(AutoSlash(Router("/products" -> routes))))
       server <- BlazeServerBuilder[F]
                  .bindHttp(config.http.port.number, config.http.host.ip)
                  .withHttpApp(httpApp.orNotFound)
